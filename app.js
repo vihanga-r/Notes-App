adc4904afbfa38f39173ac71a5dde06103e007af
Vihanga Pandigamage <vihanga.pandigamage@gmail.com>
2024-05-26 17:51:40 +0530

    Finished the app upto the authentication part

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Connect to Database
+connectDB();
+
+// static files
+app.use(express.static('public'));
+
+// templating engine
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Routes
+app.use('/', require('./server/routes/auth'));
+app.use('/', require('./server/routes/index'));
+app.use('/', require('./server/routes/dashboard'));
+
+// 404
+app.get('*', function(req, res) {
+    res.status(404).render('404')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
---
+++ server/controllers/dashboardController.js
@@ -0,0 +1,15 @@
+/**
+ * GET /
+ * Dashboard
+ */
+exports.dashboard = async (req, res) => {
+    const locals = {
+        title: "Dashboard",
+        description: "Free Nodejs Notes App",
+    }
+
+    res.render('dashboard/index', {
+        locals,
+        layout: '../views/layouts/dashboard' 
+    });
+}
