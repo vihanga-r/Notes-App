d049d7e0faffd1ebc9606f4589b96339bfec94b5
Vihanga Pandigamage <vihanga.pandigamage@gmail.com>
2024-05-27 21:03:13 +0530

    Create 'auth.js' and methods to initiate a new session, create a new user and store them in the DB.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,35 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user.js');
 
+// Create new user
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+  async function(accessToken, refreshToken, profile, done) {
+    
+    const newUser = {
+      googleId: profile.id,
+      displayName: profile.displayName,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      profileImage:profile.photos[0].value
+    }
+
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (user) {
+        done(null, user);
+      } else {
+        user = await User.create(newUser);
+        done(null, user);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 ));
 
@@ -30,12 +51,38 @@ router.get('/login-failure', (req, res) => {
   res.send('Something went wrong...')
 });
 
+// Destroy user session
+router.get('/logout', (req, res) => {
+  req.session.destroy(error => {
+    if(error) {
+      console.log(error);
+      res.send('Error logging out');
+    } else {
+      res.redirect('/')
+    }
+  })
+});
+
 // Persist user data after successful authentication
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 // Retrieve user data from session.
-passport.deserializeUser
+// passport.deserializeUser(function (id, done) {
+//   User.findOne(id, function (err, user) {
+//     done(err, user);
+//   })
+// })
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 
 module.exports = router;
