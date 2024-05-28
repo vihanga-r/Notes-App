f3907fc4dad1d32a8920cb274e5a9260df1f5aa1
Vihanga Pandigamage <vihanga.pandigamage@gmail.com>
2024-05-28 21:03:34 +0530

    Create 'Notes.js' model.

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboardController = require('../controllers/dashboardController');
 
 /**
  * Dashboard routes
  */
-router.get('/dashboard', dashboardController.dashboard);
+router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
 
 
 module.exports = router;
