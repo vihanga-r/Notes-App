cf82fba62616c5188462a8f7d5d2c14b42e602b1
Vihanga Pandigamage <vihanga.pandigamage@gmail.com>
2024-06-21 21:58:37 +0530

    Make the query to get notes from the DB and display them in the dashboard.

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -5,24 +5,44 @@ const mongoose = require('mongoose');
  * GET /
  * Dashboard
  */
+
 exports.dashboard = async (req, res) => {
+
+    let perPage = 12;
+    let page = req.query.page || 1;
+  
     const locals = {
-        title: "Dashboard",
-        description: "Free Nodejs Notes App",
-    }
-
+      title: "Dashboard",
+      description: "Free NodeJS Notes App.",
+    };
+  
     try {
-        const notes = await Note.find({});
-        
-        res.render('dashboard/index', {
-            userName: req.user.firstName,
-            locals,
-            notes,
-            layout: '../views/layouts/dashboard' 
-        });
-    } catch (error) {
-        
+      // Mongoose "^7.0.0 Update
+      const notes = await Note.aggregate([
+        { $sort: { updatedAt: -1 } },
+        { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+        {
+          $project: {
+            title: { $substr: ["$title", 0, 30] },
+            body: { $substr: ["$body", 0, 100] },
+          },
+        },
+      ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+  
+      const count = await Note.countDocuments();
+  
+      res.render('dashboard/index', {
+        userName: req.user.firstName,
+        locals,
+        notes,
+        layout: "../views/layouts/dashboard",
+        current: page,
+        pages: Math.ceil(count / perPage)
+      });
+     } catch (error) {
+      console.log(error);
     }
-
-    
-}
+  };
